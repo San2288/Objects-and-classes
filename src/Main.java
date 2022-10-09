d3b483903fb1ecf0d33ce49c62931d2322e47b92
San2288 <Soldatovaoksana65@gmail.com>
2022-10-09 21:52:32 +0800

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Author colleenMccullough = new Author("Колин", "Маккалоу");
+        Author douglasAdams = new Author("Дуглас", "Адамс");
+        Book theThornBirds = new Book("Поющие в терновнике", colleenMccullough, 1977);
+        Book theHitchhikerToTheGalaxy = new Book("Автостопом по Галактике", douglasAdams, 1979);
+        System.out.println("Название книги: " + theThornBirds.getTitle()
+                + " Автор: " + theThornBirds.getAuthor().getFullName()
+                + " Год публикации: " + theThornBirds.getPublicationYear());
+        System.out.println("Название книги: " + theHitchhikerToTheGalaxy.getTitle()
+                + " Автор: " + theHitchhikerToTheGalaxy.getAuthor().getFullName()
+                + " Год публикации: " + theHitchhikerToTheGalaxy.getPublicationYear());
     }
 }
